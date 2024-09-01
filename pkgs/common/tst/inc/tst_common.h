519b74b4e8c1612cd1a3fa282118fe5db07c149d
punchlemon <tsukiwahigashini0410@gmail.com>
2024-09-01 16:24:19 +0900

    all test is complete

---
+++ pkgs/common/tst/inc/tst_common.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tst_common.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/01 15:42:13 by retanaka          #+#    #+#             */
+/*   Updated: 2024/09/01 15:42:14 by retanaka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TST_COMMON_H
+# define TST_COMMON_H
+# include <unistd.h>
+# include "common.h"
+
+void	tst_ft_strlen(char *s, size_t len);
+
+#endif
