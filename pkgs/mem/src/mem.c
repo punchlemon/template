b9c8af98a7cad8c0ac8013214bd4c37380cf0f6b
punchlemon <tsukiwahigashini0410@gmail.com>
2024-09-08 19:41:06 +0900

    add mem pkg

--- pkgs/mem/src/mem.c
+++ pkgs/mem/src/mem.c
@@ -1,37 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   slice.c                                            :+:      :+:    :+:   */
+/*   mem.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 13:42:58 by retanaka          #+#    #+#             */
-/*   Updated: 2024/08/23 13:42:59 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/09/05 20:32:30 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "mem.h"
 #include "slice.h"
 
-int	update_cap(uint64_t len, uint64_t *cap)
-{
-	if (!cap)
-		return (-1);
-	if (*cap >= len)
-		return (0);
-	if (*cap == 0)
-		*cap = 1;
-	while (1)
-	{
-		if (*cap >= len)
-			return (1);
-		if (*cap < 1024)
-			*cap *= 2;
-		else
-			*cap *= 1.25;
-	}
-}
-
-t_slice	*create_byte_slice(uint64_t len)
+t_slice	*create_node_slice(uint64_t len)
 {
 	t_slice	*s;
 
@@ -41,17 +23,8 @@ t_slice	*create_byte_slice(uint64_t len)
 	s->l = len;
 	s->c = 1;
 	update_cap(s->l, &s->c);
-	s->p = malloc(s->c);
+	s->p = malloc(sizeof(t_node *) * s->c);
 	if (!s->p)
 		return (free(s), NULL);
 	return (s);
-}
-
-void	delete_byte_slice(t_slice *s)
-{
-	if (!s)
-		return ;
-	if (s->p)
-		free(s->p);
-	return (free(s));
 }
